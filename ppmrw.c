a0f44fef83cf49db7ede690cb0ef64e39581fe69
Robert Rasmussen <bobbyras90@gmail.com>
2016-09-10 10:54:41 -0700

    P3 file types get partially parsed. Ignores comments and gets the width and height of the image.

--- ppmrw.c
+++ ppmrw.c
@@ -9,9 +9,33 @@ typedef struct Pixel {
 Pixel* pixmap;
 FILE* fh;
 
+char* getWidthAndHeight() {
+	char checkAgain = 1;
+	char* comments = malloc(sizeof(char) * 1000);
+	
+	while (checkAgain) {
+		if (fgets(comments, 1000, fh) != NULL) {
+			if (comments[0] == '#') {
+				//fgets(comments, 1000, fh); // Ignore empty line
+			} else {
+				printf("%s", comments);
+				checkAgain = 0;
+			}
+		}
+	}
+	return comments;
+}
 
+int loadPPM3() {
+	char* widthAndHeight;
+	widthAndHeight = getWidthAndHeight();
+	printf("%s", widthAndHeight);
+	return 0;
+}
 
 int main(int argc, char* argv[]) {
+	int returnValue = 0;
+	
 	if (argc != 4) {
 		fprintf(stderr, "Usage: ppmrw target input output");
 		return 1;
@@ -22,22 +46,25 @@ int main(int argc, char* argv[]) {
 	fh = fopen(argv[2], "r");
 	if (fh == NULL) {
 		fprintf(stderr, "File not found.");
+		return 1;
 	} else {
 		if (fgets(PPMFileType, 3, fh) != NULL) {
 			int compare = strcmp(PPMFileType, "P3");
-			if (compare) {
-				printf("We found P3");
+			if (!compare) {
+				printf("We found P3\n");
+				fgets(PPMFileType, 3, fh);	// Ignore empty line
+				loadPPM3();
 			} else {
 				compare = strcmp(PPMFileType, "P6");
-				if (compare) {
-					printf("We found P6");
+				if (!compare) {
+					printf("We found P6\n");
 				} else {
-					fprintf(stderr, "Not a PPM file");
+					fprintf(stderr, "Not a PPM file\n");
 				}
 			}
 		}
 		fclose(fh);
 	}
 	
-	return 0;
+	return returnValue;
 }
