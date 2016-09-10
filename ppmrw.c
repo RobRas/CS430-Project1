2e5997be4a8ca8e5f1cce1996508511de48e5f6d
Robert Rasmussen <bobbyras90@gmail.com>
2016-09-09 19:48:23 -0700

    Updated the .gitignore and added a Pixel struct

--- ppmrw.c
+++ ppmrw.c
@@ -1,15 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct Pixel {
+	unsigned char r, g, b;
+} Pixel;
+
+Pixel* pixmap;
+
+int width, height;
+
 int main(int argc, char* argv[]) {
 	if (argc != 4) {
 		fprintf(stderr, "Usage: ppmrw target input output");
 		return 1;
 	}
 	
-	printf("%s ", argv[1]);
-	printf("%s ", argv[2]);
-	printf("%s ", argv[3]);
+	FILE* fh = fopen("output.data", "w");
+	fwrite(argv[1], sizeof(char), 1, fh);
+	fclose(fh);
 	
 	return 0;
 }
