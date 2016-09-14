fe4687322e25e889ddaa926b7a6e4154a64e4ca7
Robert Rasmussen <bobbyras90@gmail.com>
2016-09-14 11:33:04 -0700

    Further refactoring.

--- ppmrw.c
+++ ppmrw.c
@@ -17,162 +17,6 @@ int maxColorValue;
 Pixel* pixmap;
 FILE* fh;
 
-char* parseWidthAndHeight() {
-	char checkAgain = 1;
-	char* comments = malloc(sizeof(char) * 1000);
-	
-	while (checkAgain) {
-		if (fgets(comments, 1000, fh) != NULL) {
-			if (comments[0] != '#') {
-				checkAgain = 0;
-			}
-		}
-	}
-	
-	return comments;
-}
-
-int getWidthAndHeight(char* widthAndHeight) {
-	// Get Width
-	int i = 0;
-	char w[10];
-	while (isdigit(widthAndHeight[i]) && i < 10) {
-		w[i] = widthAndHeight[i];
-		i++;
-	}
-	if (i == 10) {
-		fprintf(stderr, "File width too large.");
-		return 1;
-	} else if (widthAndHeight[i] != ' ') {
-		fprintf(stderr, "Not a PPM file. Incompatible width.\n");
-		return 1;
-	} else {
-		width = atoi(w);
-		
-		// Get Height
-		i++;	// Skip space
-		int j = 0;
-		char h[8];
-		while (isdigit(widthAndHeight[i]) && j < 8) {
-			h[j] = widthAndHeight[i];
-			i++;
-			j++;
-		}
-		if (j == 8) {
-			fprintf(stderr, "File height too large.");
-			return 1;
-		} else if (widthAndHeight[i] != '\n') {
-			printf("Not a PPM File. Incompatible height.\n");
-			return 1;
-		} else {
-			height = atoi(h);
-		}
-	}
-	
-	return 0;
-}
-
-int getP3Value(unsigned char* outValue) {
-	char value[4];
-	int rgbValue;
-	
-	for (int i = 0; i < 4; i++) {
-		value[i] = fgetc(fh);
-		if (value[i] == '\n') {
-			value[i] = '\0';
-			break;
-		} else if (!isdigit(value[i])) {
-			fprintf(stderr, "Value must be a digit.");
-			return 1;
-		}
-	}
-	
-	rgbValue = atoi(value);
-	if (rgbValue > maxColorValue) {
-		fprintf(stderr, "Color value exceeding max.");
-		return 1;
-	}
-	
-	*outValue = rgbValue;
-	return 0;
-}
-
-int parseP3(int index) {
-	if (getP3Value(&pixmap[index].r)) {
-		return 1;
-	}
-	if (getP3Value(&pixmap[index].g)) {
-		return 1;
-	}
-	if (getP3Value(&pixmap[index].b)) {
-		return 1;
-	}
-	
-	return 0;
-}
-
-int parseP6() {
-	fread(pixmap, sizeof(Pixel), width * height, fh);
-	return 0;
-}
-
-int getMaxColorValue() {
-	char value[4];
-	
-	for (int i = 0; i < 4; i++) {
-		value[i] = fgetc(fh);
-		if (value[i] == '\n') {
-			value[i] = '\0';
-			break;
-		} else if (!isdigit(value[i])) {
-			fprintf(stderr, "Value must be a digit.");
-			return 1;
-		}
-	}
-	
-	maxColorValue = atoi(value);
-	
-	if (maxColorValue > 255) {
-		fprintf(stderr, "Not a PPM file. Max color value too high.\n");
-		return 1;
-	} else if (maxColorValue < 1) {
-		fprintf(stderr, "Not a PPM file. Max color value too low.\n");
-		return 1;
-	}
-	
-	return 0;
-}
-
-int loadPPM() {
-	char* widthAndHeight;
-	widthAndHeight = parseWidthAndHeight();
-	if (getWidthAndHeight(widthAndHeight)) { return 1; }
-	pixmap = malloc(sizeof(Pixel) * width * height);
-	if (getMaxColorValue()) { return 1; }
-	if (convertFrom == '3') {
-		for (int i = 0; i < width * height; i++) {
-			if (parseP3(i)) { return 1; }
-		}
-	} else {
-		if (parseP6()) { return 1; }
-	}
-	return 0;
-}
-
-int writeP3() {
-	fprintf(fh, "P3\n# Converted with Robert Rasmussen's ppmrw\n%d %d\n%d\n", width, height, maxColorValue);
-	for (int i = 0; i < width * height; i++) {
-		fprintf(fh, "%d\n%d\n%d\n", pixmap[i].r, pixmap[i].g, pixmap[i].b);
-	}
-	return 0;
-}
-
-int writeP6() {
-	fprintf(fh, "P6\n# Converted with Robert Rasmussen's ppmrw\n%d %d\n%d\n", width, height, maxColorValue);
-	fwrite(pixmap, sizeof(Pixel), width*height, fh);
-	return 0;
-}
-
 int getConversionType(char* arg) {
 	int compare = strcmp(arg, "3");
 	if (!compare) {
@@ -205,6 +49,140 @@ int getPPMFileType() {
 		}
 	}
 	
+	return 0;
+}
+
+int getWidthAndHeight() {
+	char value[20];
+	
+	while ((value[0] = fgetc(fh)) == '#') {	// Line is a comment
+		while (fgetc(fh) != '\n');	// Ignore the line
+	}
+	
+	if (!isdigit(value[0])) {
+		fprintf(stderr, "Image width is not valid.");
+		return 1;
+	}
+	
+	int i = 1;
+	while ((value[i] = fgetc(fh)) != ' ') {
+		if (!isdigit(value[i])) { 
+			fprintf(stderr, "Image width is not valid.");
+			return 1;
+		}
+		i++;
+	}
+	value[i] = '\0';
+	width = atoi(value);
+	
+	i = 0;
+	while ((value[i] = fgetc(fh)) != '\n') {
+		if (!isdigit(value[i])) {
+			fprintf(stderr, "Image width is not valid.");
+			return 1;
+		}
+		i++;
+	}
+	value[i] = '\0';
+	height = atoi(value);
+}
+
+int getMaxColorValue() {
+	char value[4];
+	
+	for (int i = 0; i < 4; i++) {
+		value[i] = fgetc(fh);
+		if (value[i] == '\n') {
+			value[i] = '\0';
+			break;
+		} else if (!isdigit(value[i])) {
+			fprintf(stderr, "Value must be a digit.");
+			return 1;
+		}
+	}
+	
+	maxColorValue = atoi(value);
+	
+	if (maxColorValue > 255) {
+		fprintf(stderr, "Not a PPM file. Max color value too high.\n");
+		return 1;
+	} else if (maxColorValue < 1) {
+		fprintf(stderr, "Not a PPM file. Max color value too low.\n");
+		return 1;
+	}
+	
+	return 0;
+}
+
+int getP3Value(unsigned char* outValue) {
+	unsigned char value[4];
+	int rgbValue;
+	
+	for (int i = 0; i < 4; i++) {
+		value[i] = fgetc(fh);
+		if (value[i] == '\n') {
+			value[i] = '\0';
+			break;
+		} else if (!isdigit(value[i])) {
+			fprintf(stderr, "Value must be a digit.");
+			return 1;
+		}
+	}
+	
+	rgbValue = atoi(value);
+	if (rgbValue > maxColorValue) {
+		fprintf(stderr, "Color value exceeding max.");
+		return 1;
+	}
+	
+	*outValue = rgbValue;
+	return 0;
+}
+
+int parseP3() {
+	for (int i = 0; i < width * height; i++) {	
+		if (getP3Value(&pixmap[i].r)) {
+			return 1;
+		}
+		if (getP3Value(&pixmap[i].g)) {
+			return 1;
+		}
+		if (getP3Value(&pixmap[i].b)) {
+			return 1;
+		}
+	}
+	
+	return 0;
+}
+
+int parseP6() {
+	fread(pixmap, sizeof(Pixel), width * height, fh);
+	return 0;
+}
+
+int loadPPM() {
+	getWidthAndHeight();
+	pixmap = malloc(sizeof(Pixel) * width * height);
+	if (getMaxColorValue()) { return 1; }
+	if (convertFrom == '3') {
+		if (parseP3()) { return 1; }
+	} else if (convertFrom == '6') {
+		if (parseP6()) { return 1; }
+	}
+	return 0;
+}
+
+int writeP3() {
+	fprintf(fh, "P3\n# Converted with Robert Rasmussen's ppmrw\n%d %d\n%d\n", width, height, maxColorValue);
+	for (int i = 0; i < width * height; i++) {
+		fprintf(fh, "%d\n%d\n%d\n", pixmap[i].r, pixmap[i].g, pixmap[i].b);
+	}
+	return 0;
+}
+
+int writeP6() {
+	fprintf(fh, "P6\n# Converted with Robert Rasmussen's ppmrw\n%d %d\n%d\n", width, height, maxColorValue);
+	fwrite(pixmap, sizeof(Pixel), width*height, fh);
 	return 0;
 }
 
